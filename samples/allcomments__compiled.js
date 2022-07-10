000be968e5cfaf1ff4bd400134809e8b58cc74fb
Varun Sethu <varun.sethu@outlook.com>
2022-07-11 00:13:00 +1000

    added all comments testcase

---
+++ samples/allcomments__compiled.js
@@ -0,0 +1,18 @@
+
+    const compiled = () => {
+        let __output = '';
+        __output += `
+These fruits are amazing:
+`; 
+__output += `
+  - <b>`; 
+__output += `s</b>
+`; 
+__output += `
+Let's see some random numbers:
+`; 
+__output += `The spaces before me should dissapear :O
+`; 
+
+        return __output;
+    }
